75e92eac1ca963988496460503d40b222d0bcd7c
rndev80 <rndev80@gmail.com>
2019-04-08 14:22:45 +0300

    Added user creation endpoint

--- InvoicedAPI/app/controllers/v1/sessions_controller.rb
+++ InvoicedAPI/app/controllers/v1/sessions_controller.rb
@@ -1,4 +1,8 @@
 class V1::SessionsController < ApplicationController
+    def show
+        current_user ? head(:ok) : head(:unauthorized)
+    end
+
     def create
         @user = User.where(email: params[:email]).first
 
@@ -10,6 +14,12 @@ class V1::SessionsController < ApplicationController
     end
 
     def destroy
+        current_user&.authentication_token = nil
 
+        if current_user&.save
+            head(:ok)
+        else
+            head(:unauthorized)
+        end
     end
 end
